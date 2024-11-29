eba801d4ae32cf661d47d391d0c41cb6802e2890
shaqcoleman <shaquille.coleman3@gmail.com>
2024-11-29 12:50:48 -0500

    Add files via upload

---
+++ Exercise05_04.java
@@ -0,0 +1,16 @@
+public class Exercise05_04 {
+    public static void main(String[] args) {
+		// Create a constant number of kilometers in a mile
+		final double KILOMETERS_PER_MILE = 1.609; 
+
+		// Display table header
+		System.out.println(
+			"Miles        Kilometers");
+
+		// Create and display table showing conversion from miles to kilometers
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf(
+				"%-13d%-10.3f\n", i, (i * KILOMETERS_PER_MILE));
+		}
+	}
+}
